f075529fbb61c966748cc4844fc79a73b387cc04
vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>
2024-09-12 16:07:27 +0530

    refactor
    Signed-off-by: vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>

--- utils/utils.go
+++ utils/utils.go
@@ -2,14 +2,17 @@ package utils
 
 import (
 	"bufio"
+	"crypto/sha256"
 	"fmt"
 	"log"
 	"os"
 	"strings"
+	config "vishalvivekm/vcs/constants"
+	"sync"
 )
 
 const (
-	logFile = "../vcs/log.txt"
+	logFile = "vcs/log.txt"
 )
 
 func CompareCommit(commitID string) (bool, error) {
@@ -59,3 +62,73 @@ func Check(e error) {
 		log.Fatalln(e)
 	}
 }
+
+func ReturnHash(content []byte) (string, error) {
+	hash := sha256.New()
+	_, err := hash.Write(content)
+	if err != nil {
+		return "", err
+	}
+	hashedString := fmt.Sprintf("%x", hash.Sum(nil))
+	return hashedString, nil
+}
+
+func ReadFileContent(name string) ([]byte, error) {
+
+	data, err := os.ReadFile(name)
+	if err != nil {
+	return nil ,fmt.Errorf("error reading file, %s, %w", name, err)
+	}
+	return data, nil
+
+}
+func WriteLogs(logMsg string) error {
+	file, err := os.OpenFile(config.LogFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	content, err := ReadFileContent(file.Name())
+	Check(err)
+	if err := os.WriteFile(file.Name(), []byte(logMsg), os.ModePerm); err != nil {
+		return err
+	}
+	if _, err = fmt.Fprint(file, "\n\n", string(content)); err != nil {
+		return err
+	}
+	return nil
+}
+
+
+func ReadIndex(wg *sync.WaitGroup, ch chan string) {
+	defer wg.Done()
+	file, err := os.Open(config.IndexFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		singleFile := scanner.Text()
+		if len(singleFile) != 0 {
+			ch <- singleFile
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		Check(err)
+	}
+	close(ch)
+}
+
+func ReadFilesAndCopy(wg *sync.WaitGroup, ch chan string, commitHash string) {
+	defer wg.Done()
+	for fileName := range ch {
+		content, err := ReadFileContent(fileName)
+		err = os.WriteFile(fmt.Sprintf("%s/%s/%s", config.CommitsDir, commitHash, fileName), content, os.ModePerm)
+		if err != nil {
+			log.Fatalf("Error writing file %s to commit: %v", fileName, err)
+		}
+		fmt.Printf("Copied file:%s to the commit %s\n", fileName, commitHash)
+	}
+}
+
